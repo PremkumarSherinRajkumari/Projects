39f0dd5f76648aa782e463f7c711f60d2f72d906
PremkumarSherinRajkumari <sherinrajkumariis@gmail.com>
2024-06-11 13:08:34 -0400

    Web Development Tools & Methods

---
+++ expense calculator/users.js
@@ -0,0 +1,26 @@
+const users = {
+
+};
+
+function isValid (username) {
+  let isValid = true;
+  isValid = isValid && !!username; // Checking if username is not empty or falsy
+  isValid = isValid && username.trim() !== ''; // Checking if username is not an empty string after trimming
+  isValid = isValid && username.match(/^[A-Za-z0-9_]+$/);
+  return isValid;
+}
+
+function getUserData (username) {
+  return users[username];
+}
+
+function addUserData (username, userData) {
+  users[username] = userData;
+}
+
+
+module.exports = {
+  isValid,
+  getUserData,
+  addUserData,
+};

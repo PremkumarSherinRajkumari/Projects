c143168b537b52e6be110d9b96b3f0bd8545b8b3
PremkumarSherinRajkumari <sherinrajkumariis@gmail.com>
2024-06-11 12:36:07 -0400

    Web Design/User Experience Engineering

--- expense calculator/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Header = () => {
-  return <h2>Expense Calculator</h2>;
-};
-
-export default Header;
--- expense calculator/src/components/Loading.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const Loading = ({ className = 'loading', children = 'Loading...' }) => {
-    return <div className={className}>{children}</div>;
-  };
-  
-  export default Loading;
